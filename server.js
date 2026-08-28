2dcc2fd91e1dacc9f826dcbac0b1a07ef8df6e76
yusufoncel708-collab <yusufoncel708@gmail.com>
2026-08-28 18:14:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
